ec09bbe779bc8a8c3ba676ab271932eaa9330219
Oleg Bryk <olegbrykmail@gmail.com>
2017-07-05 17:29:19 +0300

    Add working opencart module, and fix it

---
+++ admin/language/ua-uk/extension/module/category_with_image.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: New account name
+ * Date: 04.07.2017
+ * Time: 12:42
+ */
---
+++ admin/model/design/menu_designer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: New account name
+ * Date: 30.06.2017
+ * Time: 13:13
+ */
