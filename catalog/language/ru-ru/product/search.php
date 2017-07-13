4aed919e5026eb274564f2c0caf3df007007794e
Oleg Bryk <olegbrykmail@gmail.com>
2017-07-13 13:08:33 +0300

    Пофіксив переклад в Product/Search

--- catalog/language/ru-ru/product/search.php
+++ catalog/language/ru-ru/product/search.php
@@ -32,3 +32,4 @@ $_['text_limit']        = 'Показать:';
 // Entry
 $_['entry_search']      = 'Поиск:';
 $_['entry_description'] = 'Искать в описании товаров';
+$_['text_information_find'] ='Запрос найден в информационных страницах:';
